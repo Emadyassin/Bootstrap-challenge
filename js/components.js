046a7b929d48e26f843dcd10bff52b054fafb4a7
Emadyassin <90834368+Emadyassin@users.noreply.github.com>
2023-10-03 23:42:14 +0300

    V.01

--- js/components.js
+++ js/components.js
@@ -1,50 +1,3 @@
-/**
- * Retrieves the first element that matches the specified selector.
- *
- * @param {string} element - The CSS selector of the element to retrieve.
- * @returns {Element|null} The first element that matches the selector, or null if no element is found.
- */
-const el = (element) => document.querySelector(element);
-
-/**
- * Retrieves a list of elements that match the specified selector.
- *
- * @param {string} elements - The CSS selector of the elements to retrieve.
- * @returns {NodeList} A list of elements that match the selector.
- */
-const els = (elements) => document.querySelectorAll(elements);
-
-var path = window.location.pathname;
-var page = path.split("/").pop();
-
-const setStorage = (key, value) => {
-  localStorage.setItem(
-    key,
-    JSON.stringify({
-      data: value,
-    })
-  );
-};
-
-const getStorage = (key) => {
-  const data = localStorage.getItem(key);
-  if (data && !undefined) return JSON.parse(data).data;
-
-  return [];
-};
-
-const app = el("#app");
-
-const renderHtml = (htmlString) => {
-  app.innerHTML += htmlString;
-};
-
-var products = [];
-
-products = getStorage("shopProducts");
-
-// -----------Components-------------
-
 const navComponent = (headerItems) => {
   const elements = headerItems
     .map((item) => {
@@ -174,6 +127,59 @@ const footerComponent = () => {
     <footer class="bg-color-black text-white w-100 d-flex justify-content-center align-items-center flex-column">
         <p class="d-block">Done by Emad Yassin</p>
     </footer>
+      `;
+};
+const refreshDashboardCardComponent = (headerItems) => {
+  if (!headerItems) return "";
+  const elements = headerItems
+    .map((item) => {
+      return `
+      <div class="col-12 col-md-${12 / headerItems.length} p-3">
+        <div class="card rounded-5 h-100 overflow-hidden">
+          <img class="card-bg-img" src="../dots.png" alt="" srcset="">
+          <div class="card-body d-flex flex-column justify-content-between gap-3 text-white bg-color-black">
+              <div class="card-info d-flex flex-column ">
+                  <p class="card-text fw-medium z-1">${item.title}</p>
+                  <h5 class="card-title fw-bold text-center fa-2x z-1"> ${
+                    item.value
+                  }</h5>
+              </div>
+          </div>
+        </div>
+      </div>
+    `;
+    })
+    .join("");
+  return `
+      ${elements}   
+      `;
+};
+const dashboardCardComponent = (headerItems) => {
+  const elements = headerItems
+    .map((item) => {
+      return `
+      <div class="col-12 col-md-${12 / headerItems.length} p-3">
+        <div class="card rounded-5 h-100 overflow-hidden">
+          <img class="card-bg-img" src="../dots.png" alt="" srcset="">
+          <div class="card-body d-flex flex-column justify-content-between gap-3 text-white bg-color-black">
+              <div class="card-info d-flex flex-column ">
+                  <p class="card-text fw-medium z-1">${item.title}</p>
+                  <h5 class="card-title fw-bold text-center fa-2x z-1"> ${
+                    item.value
+                  }</h5>
+              </div>
+          </div>
+        </div>
+      </div>
+    `;
+    })
+    .join("");
+  return `
+  <div class="container">
+    <div class="row"  id="card-data">
+      ${elements} 
+    </div>  
+  </div>  
       `;
 };
 
@@ -311,7 +317,7 @@ const addProModuleComponent = () => {
                     <button type="button" class="btn btn-outline-dark" data-bs-dismiss="modal">
                         Close
                     </button>
-                    <button id="btnAddProduct" type="button" onclick="addProducts()" class="btn btn-primary" data-bs-dismiss="modal">
+                    <button id="btnAddProduct" type="button" onclick="addProducts()" class="btn btn-primary">
                         <span id="spinner" class="spinner-border-sm" aria-hidden="true"></span>
                         <span role="status">Add Product</span>
                     </button>
@@ -367,11 +373,11 @@ renderHtml(
   navComponent([
     {
       title: "Shop",
-      link: "../index.html",
+      link: "/index.html",
     },
     {
       title: "Dashboard",
-      link: "page/dashboard.html",
+      link: "/page/dashboard.html",
     },
   ])
 );
@@ -382,161 +388,22 @@ if (page === "index.html")
   renderHtml(shopMainComponent(getStorage("shopProducts")));
 
 if (page === "dashboard.html")
-  renderHtml(dashboardTableComponent(getStorage("shopProducts")));
+  renderHtml(
+    dashboardCardComponent([
+      {
+        title: "Products No",
+        value: products.length,
+      },
+      {
+        title: "Products Price",
+        value: "$ " + calcPrice(),
+      },
+    ])
+  );
+
+
+renderHtml(dashboardTableComponent(getStorage("shopProducts")));
 renderHtml(addProModuleComponent());
 renderHtml(editProModuleComponent());
 renderHtml(delProModuleComponent());
-
 renderHtml(footerComponent());
-
-// -----------Functions-------------
-
-let searchFunction = () => {
-  console.log("a");
-  var filter = "title";
-  var searchValue = document.getElementById("search-box").value;
-  var filteredData = products.filter(function (obj) {
-    return obj[filter].toUpperCase().includes(searchValue.toUpperCase());
-  });
-  document.getElementById("products").innerHTML = refreshMainComponent(filteredData);
-};
-
-var base64Image = "";
-function encodeImageFileAsURL(inputId) {
-  return new Promise(function (resolve, reject) {
-    const imageInput = document.getElementById(inputId);
-    const selectedFile = imageInput.files[0];
-    if (selectedFile) {
-      setTimeout(function () {
-        const reader = new FileReader();
-        reader.onload = function (e) {
-          base64Image = e.target.result;
-          resolve();
-        };
-        reader.readAsDataURL(selectedFile);
-      }, 2000);
-    }
-  });
-}
-
-let addProducts = () => {
-  document.getElementById("btnAddProduct").disabled = false;
-  document.getElementById("spinner").classList.add("spinner-border");
-  encodeImageFileAsURL("Product-img").then((callback) => {
-    // console.log(base64Image);
-
-    products.push({
-      title: document.getElementById("Product-name").value,
-      description: document.getElementById("Product-description").value,
-      price: Number(document.getElementById("Product-price").value),
-      image: base64Image,
-    });
-
-    document.getElementById("Product-name").value = null;
-    document.getElementById("Product-description").value = null;
-    document.getElementById("Product-price").value = null;
-    document.getElementById("Product-img").value = null;
-
-    setStorage("shopProducts", products);
-    document.getElementById("table-data").innerHTML = refreshDashboardTableComponent(getStorage("shopProducts"));
-    document.getElementById("spinner").classList.remove("spinner-border");
-    document.getElementById("btnAddProduct").removeAttribute("disabled");
-  });
-};
-
-let deleteProduct = (productsArray, index) => {
-  productsArray.splice(index, index + 1);
-  setStorage("shopProducts", products);
-  document.getElementById("table-data").innerHTML = refreshDashboardTableComponent(getStorage("shopProducts"));
-};
-
-let updateProducts = (productsArray, index) => {
-encodeImageFileAsURL("update-Product-img").then((callback) => {
-    productsArray[index].title = document.getElementById(
-      "update-Product-name"
-    ).value;
-    productsArray[index].description = document.getElementById(
-      "update-Product-description"
-    ).value;
-    productsArray[index].price = document.getElementById(
-      "update-Product-price"
-    ).value;
-    if (document.getElementById("update-Product-img").value)
-  if(base64Image)
-    productsArray[index].image =
-    base64Image;
-  
-    setStorage("shopProducts", products);
-    document.getElementById("table-data").innerHTML = refreshDashboardTableComponent(getStorage("shopProducts"));
-  });
-};
-
-let addFormToModelEditProducts = (productsArray, index) => {
-  var items = `
-      <form id="form" class="row">
-        <div class="mb-3 col-12  d-flex justify-content-end">
-          <img src="${productsArray[index].image}" class="bg-dark-subtle cart-image w-100 rounded-3 rounded-bottom-0">
-        </div>
-        <div class="mb-3 col-12 col-md-6">
-            <label for="Product-name" class="form-label">Product name</label>
-                <div class="input-group">
-                <input type="text" value="${productsArray[index].title}" class="form-control" id="update-Product-name" aria-describedby="basic-addon3 basic-addon4">
-            </div>
-        </div>
-        <div class="mb-3 col-12 col-md-6">
-            <label for="Product-description" class="form-label">Product description</label>
-                <div class="input-group">
-                <input type="text" value="${productsArray[index].description}" class="form-control" id="update-Product-description" aria-describedby="basic-addon3 basic-addon4">
-            </div>
-        </div>
-        <div class="mb-3 col-12 col-md-6">
-            <label for="Product-price" class="form-label">Product price</label>
-            <div class="input-group">
-                <input type="number" value="${productsArray[index].price}" class="form-control" id="update-Product-price" aria-describedby="basic-addon3 basic-addon4">
-            </div>
-        </div>
-        <div class="mb-3 col-12 col-md-6">
-            <label for="Product-img" class="form-label">Image URL</label>
-            <div class="input-group">
-                <input type="file" value="${productsArray[index].image}" class="form-control" id="update-Product-img" aria-describedby="basic-addon3 basic-addon4">
-            </div>
-        </div>
-      </form>
-    `;
-  if (items) {
-    document.getElementById("model-edit-products").innerHTML = items;
-  }
-};
-
-let addButtonsToAlertDeleteProducts = (productsArray, index) => {
-  var items = `   
-        <button type="button" class="btn btn-outline-dark" data-bs-dismiss="modal">
-            No
-        </button> 
-        <button type="button" onclick="deleteProduct(products ,${index})" data-bs-dismiss="modal" class="btn bg-danger text-white">
-            Yse
-        </button>
-    `;
-  if (items) {
-    document.getElementById("modal-confirm-delete-buttons").innerHTML = items;
-  }
-};
-let addButtonsToEditProducts = (productsArray, index) => {
-  var items = `   
-        <button type="button" class="btn btn-outline-dark" data-bs-dismiss="modal">
-            Close
-        </button>
-        <button id="btnAddProduct" type="button" onclick="updateProducts(products ,${index})" class="btn btn-primary" data-bs-dismiss="modal">
-            <span id="spinner" class="spinner-border-sm" aria-hidden="true"></span>
-            <span role="status">Save Change</span>
-        </button>
-    `;
-  if (items) {
-    document.getElementById("modal-confirm-edit-buttons").innerHTML = items;
-  }
-};
-
-if (page === "index.html") {
-  const searchBoxValue = el(".search-box");
-  searchBoxValue.addEventListener("input", searchFunction);
-}
---
+++ js/functions.js
@@ -0,0 +1,170 @@
+let searchFunction = () => {
+  console.log("a");
+  var filter = "title";
+  var searchValue = document.getElementById("search-box").value;
+  var filteredData = products.filter(function (obj) {
+    return obj[filter].toUpperCase().includes(searchValue.toUpperCase());
+  });
+  document.getElementById("products").innerHTML =
+    refreshMainComponent(filteredData);
+};
+
+let calcPrice = () => {
+  total = 0;
+  products.forEach((element) => {
+    total += element.price;
+  });
+  return total;
+};
+
+var base64Image = "";
+function encodeImageFileAsURL(inputId) {
+  return new Promise(function (resolve, reject) {
+    const imageInput = document.getElementById(inputId);
+    const selectedFile = imageInput.files[0];
+    if (selectedFile) {
+      setTimeout(function () {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+          base64Image = e.target.result;
+          resolve();
+        };
+        reader.readAsDataURL(selectedFile);
+      }, 2000);
+    }
+  });
+}
+
+let addProducts = () => {
+  document.getElementById("btnAddProduct").disabled = false;
+  document.getElementById("spinner").classList.add("spinner-border");
+  encodeImageFileAsURL("Product-img").then((callback) => {
+    // console.log(base64Image);
+
+    products.push({
+      title: document.getElementById("Product-name").value,
+      description: document.getElementById("Product-description").value,
+      price: Number(document.getElementById("Product-price").value),
+      image: base64Image,
+    });
+
+    document.getElementById("Product-name").value = null;
+    document.getElementById("Product-description").value = null;
+    document.getElementById("Product-price").value = null;
+    document.getElementById("Product-img").value = null;
+
+    setStorage("shopProducts", products);
+    document.getElementById("table-data").innerHTML =
+      refreshDashboardTableComponent(getStorage("shopProducts"));
+    // document.getElementById("card-data").innerHTML =
+    //   refreshDashboardCardComponent(getStorage("shopProducts"));
+    document.getElementById("spinner").classList.remove("spinner-border");
+    document.getElementById("btnAddProduct").removeAttribute("disabled");
+
+    var myModal = document.getElementById("addProductModal");
+    var modal = new bootstrap.Modal(myModal);
+    modal.hide();
+  });
+};
+
+let deleteProduct = (productsArray, index) => {
+  productsArray.splice(index, index + 1);
+  setStorage("shopProducts", products);
+  document.getElementById("table-data").innerHTML =
+    refreshDashboardTableComponent(getStorage("shopProducts"));
+
+//   document.getElementById("card-data").innerHTML =
+//     refreshDashboardCardComponent(getStorage("shopProducts"));
+};
+
+let updateProducts = (productsArray, index) => {
+  encodeImageFileAsURL("update-Product-img").then((callback) => {
+    productsArray[index].title = document.getElementById(
+      "update-Product-name"
+    ).value;
+    productsArray[index].description = document.getElementById(
+      "update-Product-description"
+    ).value;
+    productsArray[index].price = document.getElementById(
+      "update-Product-price"
+    ).value;
+    if (document.getElementById("update-Product-img").value)
+      if (base64Image) productsArray[index].image = base64Image;
+
+    setStorage("shopProducts", products);
+    document.getElementById("table-data").innerHTML =
+      refreshDashboardTableComponent(getStorage("shopProducts"));
+    // document.getElementById("card-data").innerHTML =
+    //   refreshDashboardCardComponent(getStorage("shopProducts"));
+  });
+};
+
+let addFormToModelEditProducts = (productsArray, index) => {
+  var items = `
+        <form id="form" class="row">
+          <div class="mb-3 col-12  d-flex justify-content-end">
+            <img src="${productsArray[index].image}" class="bg-dark-subtle cart-image w-100 rounded-3 rounded-bottom-0">
+          </div>
+          <div class="mb-3 col-12 col-md-6">
+              <label for="Product-name" class="form-label">Product name</label>
+                  <div class="input-group">
+                  <input type="text" value="${productsArray[index].title}" class="form-control" id="update-Product-name" aria-describedby="basic-addon3 basic-addon4">
+              </div>
+          </div>
+          <div class="mb-3 col-12 col-md-6">
+              <label for="Product-description" class="form-label">Product description</label>
+                  <div class="input-group">
+                  <input type="text" value="${productsArray[index].description}" class="form-control" id="update-Product-description" aria-describedby="basic-addon3 basic-addon4">
+              </div>
+          </div>
+          <div class="mb-3 col-12 col-md-6">
+              <label for="Product-price" class="form-label">Product price</label>
+              <div class="input-group">
+                  <input type="number" value="${productsArray[index].price}" class="form-control" id="update-Product-price" aria-describedby="basic-addon3 basic-addon4">
+              </div>
+          </div>
+          <div class="mb-3 col-12 col-md-6">
+              <label for="Product-img" class="form-label">Image URL</label>
+              <div class="input-group">
+                  <input type="file" value="${productsArray[index].image}" class="form-control" id="update-Product-img" aria-describedby="basic-addon3 basic-addon4">
+              </div>
+          </div>
+        </form>
+      `;
+  if (items) {
+    document.getElementById("model-edit-products").innerHTML = items;
+  }
+};
+
+let addButtonsToAlertDeleteProducts = (productsArray, index) => {
+  var items = `   
+          <button type="button" class="btn btn-outline-dark" data-bs-dismiss="modal">
+              No
+          </button> 
+          <button type="button" onclick="deleteProduct(products ,${index})" data-bs-dismiss="modal" class="btn bg-danger text-white">
+              Yse
+          </button>
+      `;
+  if (items) {
+    document.getElementById("modal-confirm-delete-buttons").innerHTML = items;
+  }
+};
+let addButtonsToEditProducts = (productsArray, index) => {
+  var items = `   
+          <button type="button" class="btn btn-outline-dark" data-bs-dismiss="modal">
+              Close
+          </button>
+          <button id="btnAddProduct" type="button" onclick="updateProducts(products ,${index})" class="btn btn-primary" data-bs-dismiss="modal">
+              <span id="spinner" class="spinner-border-sm" aria-hidden="true"></span>
+              <span role="status">Save Change</span>
+          </button>
+      `;
+  if (items) {
+    document.getElementById("modal-confirm-edit-buttons").innerHTML = items;
+  }
+};
+
+if (page === "index.html") {
+  const searchBoxValue = el(".search-box");
+  searchBoxValue.addEventListener("input", searchFunction);
+}
